c55630ec3ddc5692999146259292ccad9ed7276a
Heiko <heiko.holland@me.com>
2024-10-27 14:10:44 +0100

    add basic game mechanics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,66 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { JsonPipe } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { Word, words } from './words';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [JsonPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'game3';
+  guessesAllowed = 6;
+  wrongGuesses = 0;
+  lettersGuessed: string[] = [];
+
+  words: Word[] = words.map((word) => {
+    return { ...word, letters: word.letters.toLocaleUpperCase() };
+  });
+
+  keys = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÜ'.split('');
+  word: Word = this.words[Math.floor(Math.random() * this.words.length)];
+  lettersNeeded = this.word.letters
+    .split('')
+    .filter((letter, index, self) => self.indexOf(letter) === index);
+
+  @HostListener('document:keypress', ['$event'])
+  onKeyClick(event: KeyboardEvent) {
+    const key = event.key.toLocaleUpperCase();
+    console.log('Key clicked:', key);
+
+    if (this.isWon() || this.isLost()) {
+      console.log('Game is over');
+      return;
+    }
+
+    if (this.letterIsGuessed(key)) {
+      console.log('Letter already guessed');
+      return;
+    }
+
+    if (!this.lettersNeeded.includes(key)) {
+      this.wrongGuesses++;
+      console.log('Wrong guess');
+    }
+
+    this.lettersGuessed.push(key);
+    console.log('lettersGuessed:', this.lettersGuessed);
+  }
+
+  isWon() {
+    return this.lettersNeeded.every((letter) => this.letterIsGuessed(letter));
+  }
+
+  isLost() {
+    return this.wrongGuesses >= this.guessesAllowed;
+  }
+
+  letterIsGuessed(letter: string) {
+    return this.lettersGuessed.includes(letter);
+  }
+
+  letterIsNeeded(letter: string) {
+    return this.lettersNeeded.includes(letter);
+  }
 }
